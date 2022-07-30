8352d6f0329aebcc6ba0768d0ea68f95b364d70a
yuvaraj-i <yuvarajyosef@gmail.com>
2022-07-30 13:12:13 +0530

    Update changed reading file

--- src/main/scala/com/presidio/App.scala
+++ src/main/scala/com/presidio/App.scala
@@ -1,29 +1,36 @@
 package com.presidio
 
-import epic.models.{ParserSelector, PosTagSelector}
+import epic.models.{NerSelector, PosTagSelector}
 import epic.preprocess.{MLSentenceSegmenter, TreebankTokenizer}
+
+import scala.io.Source
 
 object App {
   def main(args: Array[String]): Unit = {
+    val URL = "https://norvig.com/big.txt"
 
-    val text: String = "hi my name i yuvaraj"
+    val sourceConnection = Source.fromURL(URL)
+    val text = sourceConnection.mkString
 
     val sentenceSplitter = MLSentenceSegmenter.bundled().get
     val tokenizer = new TreebankTokenizer()
     val sentences = sentenceSplitter(text).map(tokenizer).toIndexedSeq
+    val tagger = PosTagSelector.loadTagger("en").get
+    val ner = NerSelector.loadNer("en").get
+//    val ner = Segmenter.nerSystem(NerSelector.loadNer("en").get.asInstanceOf[SemiCRF[String, String]])
 
-
-    for(sentence <- sentences) {
-      println(sentence)
-
-      val tagger = PosTagSelector.loadTagger("en").get
+    sentences.foreach(sentence =>  {
+      println(sentence.mkString(" "))
 
       val tags = tagger.bestSequence(sentence)
-
       println(tags.render)
-    }
 
+      println()
 
+      val entity = ner.bestSequence(sentence)
+      println(entity)
+    })
 
+    sourceConnection.close()
   }
 }
